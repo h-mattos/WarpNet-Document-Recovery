feb80355681fa54567e34065cab9e8b31409ca4f
Joshua Batkhan <jbatkhan3@gatech.edu.com>
2025-07-19 17:26:28 -0400

    adding function to pad or crop psf kernels to a standard size and save out to folder

--- src/generate_blurred_images.py
+++ src/generate_blurred_images.py
@@ -6,12 +6,14 @@ from tqdm import tqdm
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
-from src.utils.blur_utils import apply_psf_blur
+from src.utils.blur_utils import apply_psf_blur, pad_or_crop_kernel
 
 input_image_dir = 'data/warped'
 input_psf_dir = 'data/BMVC_image_data/psf'
+output_normalized_psf_dir = 'data/normalized_psf'
 output_blur_dir = 'data/blurred'
 
+os.makedirs(output_normalized_psf_dir, exist_ok=True)
 os.makedirs(output_blur_dir, exist_ok=True)
 
 for filename in tqdm(os.listdir(input_image_dir)):
@@ -19,11 +21,14 @@ for filename in tqdm(os.listdir(input_image_dir)):
     img_id = filename.replace("_orig.png", "")
     image_path = os.path.join(input_image_dir, filename)
     psf_path = os.path.join(input_psf_dir, f"{img_id}_psf.png")
-    output_path = os.path.join(output_blur_dir, f"{img_id}_blur.png")
-
+    output_path_psf = os.path.join(output_normalized_psf_dir, f"{img_id}_psf.png")
+    output_path_images = os.path.join(output_blur_dir, f"{img_id}_blur.png")
+    
     image = cv2.imread(image_path)
-    psf_kernel = cv2.imread(psf_path)
+    psf_kernel = pad_or_crop_kernel(cv2.imread(psf_path))
+
+    cv2.imwrite(output_path_psf, psf_kernel)
 
     blurred = apply_psf_blur(image, psf_kernel)
 
-    cv2.imwrite(output_path, blurred)
+    cv2.imwrite(output_path_images, blurred)
--- src/utils/blur_utils.py
+++ src/utils/blur_utils.py
@@ -1,6 +1,32 @@
 import cv2
 import numpy as np
 
+def pad_or_crop_kernel(psf, target_size = 20):
+    h, w, _ = psf.shape
+    
+    pad_h = max(0, target_size - h)
+    pad_w = max(0, target_size - w)
+    
+    if pad_h > 0 or pad_w > 0:
+        psf = np.pad(
+            psf,
+            pad_width = (
+                (pad_h // 2, pad_h - pad_h // 2),
+                (pad_w // 2, pad_w - pad_w // 2),
+                (0, 0)
+            ),
+            mode = "constant"
+        )
+
+    h, w, _ = psf.shape
+    crop_h = h - target_size
+    crop_w = w - target_size
+    h_start = crop_h // 2
+    w_start = crop_w // 2
+    
+    result = psf[h_start : h_start + target_size, w_start : w_start + target_size, :]
+    return result
+
 def apply_psf_blur(image, psf_kernel):
     blurred_channels = []
     
